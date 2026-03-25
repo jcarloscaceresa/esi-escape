126d49fad4586ecdbe3a3054768f6bfbe04c69d5
Lacamajo <165323107+Lacamajo@users.noreply.github.com>
2026-03-25 16:14:50 +0100

    Update memoria.h

--- memoria.h
+++ memoria.h
@@ -3,8 +3,8 @@
 #include "ficheros.h"
 
 // Funciones para volcar información de ficheros a memoria dinámica
-void cargarDatosMemoria(Partida *estado);
-void guardarDatosFicheros(Partida *estado);
-void liberarDatosMemoria(Partida *estado);
+void cargarDatosMemoria(Partida *Estado);
+void guardarDatosFicheros(Partida *Estado);
+void liberarDatosMemoria(Partida *Estado);
 
 #endif
